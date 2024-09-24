4877e92c1b3193aa260614c5b5f18328a9beec97
Michael Smith <throwawayclover@gmail.com>
2024-09-24 00:31:42 -0400

    wip: commit progress on challenge 3

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
@@ -12,14 +13,32 @@ type PokedexCommand struct {
 }
 
 func PokedexCommands() map[string]PokedexCommand {
-	var cmdMap map[string]PokedexCommand
-
+	mapOffset := 0
 	exit := func() error {
 		os.Exit(0)
 		return nil
 	}
 
+	var cmdMap map[string]PokedexCommand
 	cmdMap = map[string]PokedexCommand{
+		"map": {
+			command:     "map",
+			description: "List 20 regions. The next call to map will list the next 20 regions (or however many remain).",
+			operation: func() error {
+				return nil
+			},
+		},
+		"mapb": {
+			command:     "mapb",
+			description: "List the previous 20 regions.",
+			operation: func() error {
+				if mapOffset == 0 {
+					return errors.New("can't go back any further in map list")
+				}
+
+				return nil
+			},
+		},
 		"exit": {
 			command:     "exit",
 			description: "Exit the program",
@@ -57,13 +76,18 @@ func main() {
 		fmt.Print(("pokedex> "))
 		fmt.Scanln(&input)
 
-		if cmd, ok := cmdMap[input]; ok {
-			err := cmd.operation()
-			if err != nil {
+		cmd, ok := cmdMap[input]
+		if !ok {
+			fmt.Println("Sorry, don't know that command")
+			continue
+		}
+
+		err := cmd.operation()
+		if err != nil {
+			fmt.Printf("error - %v\n", err)
+			if input == "exit" || input == "quit" {
 				os.Exit(1)
 			}
-		} else {
-			fmt.Println("Sorry, don't know that command")
 		}
 	}
 }
